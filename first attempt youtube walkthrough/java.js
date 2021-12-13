21b003d6d961329664ff69f1e20b39bedc0953e7
Keiren-b <keiren_b@hotmail.com>
2021-12-13 14:50:16 +0100

    youtube walkthrough

--- first attempt youtube walkthrough/java.js
+++ first attempt youtube walkthrough/java.js
@@ -1,14 +1,21 @@
-const container = document.querySelector('.container')
-const btnBlack = document.createElement('button')
-const btnRgb = document.createElement('button')
-const btnGray = document.createElement('button')
-const btnSize = document.createElement('button')
+const container = document.querySelector('.container');
+const btnBlack = document.createElement('button');
+const btnRgb = document.createElement('button');
+const btnGray = document.createElement('button');
+const section = document.querySelector('.section');
+const btnSize = document.createElement('button');
 const buttonsContainer = document.querySelector('.buttons');
+
+window.onload = ()=> {}
+    const boxs = container.querySelectorAll('.box')
+    boxs.forEach(box => box.addEventListener('mouseover', () =>{
+        box.style.background = 'black'
+    }))
 
 function createDivs(col, rows) {
     for(let i = 0; i <(col * rows); i++){
        const div = document.createElement('div')
-       div.style.border = '1px solid red'
+    //    div.style.border = '1px solid red'
        container.style.grideTemplateColumns = `repeat(${col}, ifr)`;
        container.style.grideTemplateRows = `repeat(${rows}, ifr)`;
        container.appendChild(div).classList.add('box')
@@ -90,3 +97,28 @@ function reSize(){
     buttonsContainer.appendChild(btnSize).classList.add('btn')
 }
 reSize()
+
+function backgroundAnim(){
+    colors = ['red', 'blue', 'yellow', 'green']
+    const section = document.querySelector('.section')
+    const span = document.createElement('span')
+
+    let size = Math.random() * 50;
+
+    span.style.width = 10 + size + 'px'
+    span.style.height = 10 + size + 'px'
+
+    span.style.borderRadius = `${size}%`
+    span.style.top = Math.random() * innerHeight + 'px'
+    span.style.left = Math.random() * innerHeight + 'px'
+
+    const backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+
+    span.style.background = backgroundColor;
+    section.appendChild(span)
+
+    setTimeout(() => {span.remove()}, 5000)
+}
+setInterval(backgroundAnim, 500);
+
+
